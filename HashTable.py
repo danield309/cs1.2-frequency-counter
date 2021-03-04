a6bdb699fe18fcbc2ecd7c31cf4dc7f98adb8cbb
Daniel Duque <duque1daniel@gmail.com>
2021-03-03 23:02:25 -0800

    completed HashTable.py

--- HashTable.py
+++ HashTable.py
@@ -6,37 +6,41 @@ class HashTable:
     self.size = size
     self.arr = self.create_arr(size)
 
-
   # 1️⃣ TODO: Complete the create_arr method.
 
   # Each element of the hash table (arr) is a linked list.
   # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
 
-  def create_arr(self, size):
+  def create_arr(self, size): # creates array of a given size and fills elements with a ll object
+    arr = []
 
-    pass
+    for i in range(size):
+      ll = LinkedList()
+      arr.append(ll)
 
-
-
+    return arr
 
   # 2️⃣ TODO: Create your own hash function.
 
   # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored.
 
-  def hash_func(self, key):
-
-    pass
-
+  def hash_func(self, key): # creates keys into index values were we it can decide were to store key value pairs
+    index = hash(key) % self.size
+    return index
 
   # 3️⃣ TODO: Complete the insert method.
 
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
-  def insert(self, key, value):
-    pass
+  def insert(self, key, value): # generates a hashed key, assings to a touple, checks for duplicates, if found it will place in a ll, then we run replace to remove the past duplicate
+    hashKey = self.hash_func(key)
+    bit = (key, value)
+    newBit = self.arr[hashKey].find(key)
 
-
-
+    if newBit != -1:
+      new_item = self.arr[hashKey].append(newBit)
+      self.arr[hashKey].replace(new_item)
+    else: self.arr[hashKey].append(bit)
 
   # 4️⃣ TODO: Complete the print_key_values method.
 
@@ -49,5 +53,6 @@ class HashTable:
   # blooms: 1
   # erase: 2
 
-  def print_key_values(self):
-    pass
+  def print_key_values(self): # goes throughout all the table and prints all data to the command line
+    for i in range(self.size):
+      self.arr[i].print_nodes()
